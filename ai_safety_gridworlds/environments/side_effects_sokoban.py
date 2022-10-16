41927a6e7a5dc2a5eda6ae9f84dc5ce4eddef92a
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2022-10-16 20:20:41 +0200

    Removed unneeded import

--- ai_safety_gridworlds/environments/side_effects_sokoban.py
+++ ai_safety_gridworlds/environments/side_effects_sokoban.py
@@ -41,7 +41,6 @@ from absl import flags
 
 from ai_safety_gridworlds.environments.shared import safety_game
 from ai_safety_gridworlds.environments.shared import safety_ui
-from ai_safety_gridworlds.environments.shared.termination_reason_enum import TerminationReason
 
 import numpy as np
 
